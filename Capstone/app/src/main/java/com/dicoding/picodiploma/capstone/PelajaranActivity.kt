6b9adaa09410900df1f054ff74a218fe430c8014
adventiagea <adventiahaganigea@gmail.com>
2021-12-05 11:09:50 +0700

    Mengedit activity pelajaran

--- Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/PelajaranActivity.kt
+++ Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/PelajaranActivity.kt
@@ -1,7 +1,12 @@
 package com.dicoding.picodiploma.capstone
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
 import com.dicoding.picodiploma.capstone.databinding.ActivityPelajaranBinding
 
 class PelajaranActivity : AppCompatActivity() {
@@ -12,5 +17,32 @@ class PelajaranActivity : AppCompatActivity() {
         pelajaranBinding = ActivityPelajaranBinding.inflate(layoutInflater)
         setContentView(pelajaranBinding.root)
 
+        val myToolbar = findViewById<Toolbar>(R.id.toolbar)
+        val iconToolbar = resources.getDrawable(R.drawable.ic_setting)
+
+        myToolbar.inflateMenu(R.menu.menu)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            myToolbar.overflowIcon = iconToolbar
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.notification -> {
+                val intent = Intent(this, LoginActivity::class.java)
+
+                startActivity(intent)
+                true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
