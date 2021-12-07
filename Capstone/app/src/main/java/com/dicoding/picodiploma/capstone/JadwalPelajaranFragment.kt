023e0d0d697a06a9783a8ce27830e01136ac289a
adventiagea <adventiahaganigea@gmail.com>
2021-12-07 18:05:35 +0700

    mengkonfigurasi fragment jadwal pelajaran

--- Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/JadwalPelajaranFragment.kt
+++ Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/JadwalPelajaranFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dicoding.picodiploma.capstone.databinding.FragmentJadwalPelajaranBinding
+import com.dicoding.picodiploma.capstone.databinding.FragmentPengumumanBinding
 
 class JadwalPelajaranFragment : Fragment() {
+    private var pelajaranBinding: FragmentJadwalPelajaranBinding?= null
+    private val binding get() = pelajaranBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jadwal_pelajaran, container, false)
+    ): View {
+        pelajaranBinding = FragmentJadwalPelajaranBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 
