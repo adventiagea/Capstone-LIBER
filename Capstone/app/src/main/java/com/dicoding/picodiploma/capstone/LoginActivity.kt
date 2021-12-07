f0d49fcfd6b1ea0316345afd448d9853709d6310
adventiagea <adventiahaganigea@gmail.com>
2021-12-07 13:48:52 +0700

    mengkonfigurasi fitur login

--- Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/LoginActivity.kt
+++ Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/LoginActivity.kt
@@ -3,10 +3,14 @@ package com.dicoding.picodiploma.capstone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.dicoding.picodiploma.capstone.databinding.ActivityLoginBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginBinding : ActivityLoginBinding
+    private lateinit var loginDB : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +18,51 @@ class LoginActivity : AppCompatActivity() {
         setContentView(loginBinding.root)
 
         loginBinding.button.setOnClickListener {
-            val intent = Intent(this, UserActivity::class.java)
+            val username = loginBinding.UsernameInput.text.toString()
 
-            startActivity(intent)
+            if (username.isNotEmpty()){
+
+                readDataUsername(username)
+            }
+            else {
+                Toast.makeText(this, "Please enter username and password!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    private fun readDataUsername(username: String){
+        loginDB = FirebaseDatabase.getInstance().getReference("User")
+
+        val usernameInput = loginBinding.UsernameInput.text.toString()
+        val passwordInput = loginBinding.editTextTextPassword2.text.toString()
+
+        loginDB.child(username).get().addOnSuccessListener {
+            if (it.exists()) {
+                val usernameData = it.child("id").value.toString()
+                val passwordData = it.child("pass").value.toString()
+
+
+                if (usernameInput==usernameData && passwordInput == passwordData){
+                    val intent = Intent(this, UserActivity::class.java)
+
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this, "Username or Password is wrong!", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+            else {
+                Toast.makeText(this, "Username doesn't exist!", Toast.LENGTH_SHORT).show()
+
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Username doesn't exist!", Toast.LENGTH_SHORT).show()
         }
     }
+
 }
