5d3b7a84f3b4852c7ef5ab23f14063a7e3364b34
adventiagea <adventiahaganigea@gmail.com>
2021-12-07 20:43:39 +0700

    Mengubah SectionsPagerAdapter.kt

--- Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/SectionsPagerAdapter.kt
+++ Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/SectionsPagerAdapter.kt
@@ -1,10 +1,13 @@
 package com.dicoding.picodiploma.capstone
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class SectionsPagerAdapter (activity:AppCompatActivity) : FragmentStateAdapter(activity){
+class SectionsPagerAdapter (activity:AppCompatActivity, bundle: Bundle) :
+    FragmentStateAdapter(activity){
+    private val myBundle = bundle
 
     override fun createFragment(position: Int): Fragment {
         var fragment: Fragment? = null
@@ -12,10 +15,10 @@ class SectionsPagerAdapter (activity:AppCompatActivity) : FragmentStateAdapter(a
             0 -> fragment = JadwalPelajaranFragment()
             1 -> fragment = PengumumanFragment()
         }
+
+        fragment?.arguments = this.myBundle
         return fragment as Fragment
     }
 
-    override fun getItemCount(): Int {
-        return 2
-    }
+    override fun getItemCount(): Int = 2
 }
