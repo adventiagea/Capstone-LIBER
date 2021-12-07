afb956a2d2ad7ae4dae2f7359ad1a840328a0c3e
adventiagea <adventiahaganigea@gmail.com>
2021-12-07 13:48:19 +0700

    membuat data class Pelajaran

---
+++ Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/PelajaranData.kt
@@ -0,0 +1,8 @@
+package com.dicoding.picodiploma.capstone
+
+data class PelajaranData(
+    val class_name : String,
+    val class_code : String,
+    val class_lecture : String,
+    val class_time : String
+)
