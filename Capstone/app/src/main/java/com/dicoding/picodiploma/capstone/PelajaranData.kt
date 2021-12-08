8ccc8b4b6f423c2d72de92e41b8e75ca91d48b7c
adventiagea <adventiahaganigea@gmail.com>
2021-12-09 00:35:08 +0700

    Mengubah PelajaranData.kt

--- Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/PelajaranData.kt
+++ Capstone/app/src/main/java/com/dicoding/picodiploma/capstone/PelajaranData.kt
@@ -1,9 +1,10 @@
 package com.dicoding.picodiploma.capstone
 
 data class PelajaranData(
-    val class_name: String,
-    val class_code: String,
-    val class_lecture: String,
-    val class_time: String,
-    val class_date: String
+    val matkul: String ?= null,
+    val Kelas: String ?= null,
+    val Nama: String ?= null,
+    val Jam: String ?= null,
+    val Hari : String ?= null,
+    val Pertemuan : Int ?= null
 )
